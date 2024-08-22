ce5fc8bbe05af512ff713d6b5dddee6c58580d0a
maynat123 <maynatrajput511999@gmail.com>
2024-08-21 22:03:05 -0700

    user management table

--- src/Components/AdminManagement.js
+++ src/Components/AdminManagement.js
@@ -2,7 +2,82 @@ import React from 'react'
 
 const AdminManagement = () => {
   return (
-    <div>AdminManagement</div>
+    <div className='container mx-auto p-4'>
+
+
+      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+          <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white ">
+            Users List
+            
+          </caption>
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Full Name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Email
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Password
+              </th>
+              <th scope="col" class="px-6 py-3">
+                <span class="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b  ">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                Apple MacBook Pro 17"
+              </th>
+              <td class="px-6 py-4">
+                Silver
+              </td>
+              <td class="px-6 py-4">
+                Laptop
+              </td>
+              <td scope="col" class="px-6 py-3 flex justify-center">
+                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Edit</button>
+                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Delete</button>
+              </td>
+            </tr>
+            <tr class="bg-white border-b ">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                Microsoft Surface Pro
+              </th>
+              <td class="px-6 py-4">
+                White
+              </td>
+              <td class="px-6 py-4">
+                Laptop PC
+              </td>
+              <td scope="col" class="px-6 py-3 flex justify-center">
+                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Edit</button>
+                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Delete</button>
+              </td>
+            </tr>
+            <tr class="bg-white ">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap ">
+                Magic Mouse 2
+              </th>
+              <td class="px-6 py-4">
+                Black
+              </td>
+              <td class="px-6 py-4">
+                Accessories
+              </td>
+              <td scope="col" class="px-6 py-3 flex justify-center">
+                <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Edit</button>
+                <button type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">Delete</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+    </div>
   )
 }
 
